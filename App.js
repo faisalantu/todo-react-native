eb2ecdf62c43ca96133c9bc9b304b494af5f5b72
faisalantu <faisalantu@gmail.com>
2022-06-17 14:09:11 +0600

    🅰️ text input design done

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import tw from "twrnc";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Layout>
+      <View>
+        <View style={tw`rounded-lg bg-gray-100 p-4`}>
+          <TextInput
+            placeholder='Task Title'
+            style={tw`rounded-md h-12 w-full px-4 border border-gray-400 bg-white`}
+          ></TextInput>
+          <TextInput
+            placeholder='Description'
+            multiline={true}
+            style={tw`rounded-md h-24 w-full px-4 pt-2 border border-gray-400 bg-white mt-5`}
+          ></TextInput>
+          <View style={tw`mt-5 flex items-end`}>
+            <Pressable style={tw`px-3 py-3 bg-gray-800 rounded flex justify-center items-center w-4/12`}>
+              <Text style={tw`text-white font-semibold`}>CREATE</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Layout>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
