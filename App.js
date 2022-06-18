889ee9616b3d6838e8c808324508f9192be8f7fa
faisalantu <faisalantu@gmail.com>
2022-06-18 12:43:52 +0600

    🎉 implemented react screen and react navigation

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
-import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
-import tw from "twrnc";
-import Layout from "./components/Layout";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './Screens/HomeScreen';
+import LoginScreen from './Screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Layout>
-      <View>
-        <View style={tw`rounded-lg bg-gray-100 p-4`}>
-          <TextInput
-            placeholder='Task Title'
-            style={tw`rounded-md h-12 w-full px-4 border border-gray-400 bg-white`}
-          ></TextInput>
-          <TextInput
-            placeholder='Description'
-            multiline={true}
-            style={tw`rounded-md h-24 w-full px-4 pt-2 border border-gray-400 bg-white mt-5`}
-          ></TextInput>
-          <View style={tw`mt-5 flex items-end`}>
-            <Pressable style={tw`px-3 py-3 bg-gray-800 rounded flex justify-center items-center w-4/12`}>
-              <Text style={tw`text-white font-semibold`}>CREATE</Text>
-            </Pressable>
-          </View>
-        </View>
-      </View>
-    </Layout>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{headerShown:false}} name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
