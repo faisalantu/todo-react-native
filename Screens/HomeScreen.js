8a5d626b0240f548c864441c00b45f6410cb9520
faisalantu <faisalantu@gmail.com>
2022-06-21 15:39:25 +0600

    🏠 Home Screen updated

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,54 +1,45 @@
-import {
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  ScrollView,
-  SafeAreaView,
-} from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import tw from "twrnc";
 import Layout from "./../components/Layout";
-// import auth from "../firebase.init";
-// import { useAuthState } from "react-firebase-hooks/auth";
+import axios from "../axiosConfig";
+import auth from "../firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
 import Card from "../components/Card";
-
+import { useQuery } from "react-query";
+import TaskInput from "../components/TaskInput";
 const HomeScreen = () => {
-  // const [user, loading, error] = useAuthState(auth);
-  // console.log(user.email);
+  const [user, loading, error] = useAuthState(auth);
+
+  const {
+    isLoading,
+    data: todos,
+    refetch,
+  } = useQuery("todos", async () => {
+    const res = await axios.get(`/todo?userEmail=${user.email}`);
+    return res.data;
+  });
+
+  if (isLoading)
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+
   return (
     <Layout>
       <View style={tw`flex-1`}>
-        <View>
-          <View style={tw`rounded-lg bg-gray-100 p-4`}>
-            <TextInput
-              placeholder='Task Title'
-              style={tw`rounded-md h-12 w-full px-4 border border-gray-400 bg-white`}
-            ></TextInput>
-            <TextInput
-              placeholder='Description'
-              multiline={true}
-              style={tw`rounded-md h-24 w-full px-4 pt-2 border border-gray-400 bg-white mt-5`}
-            ></TextInput>
-            <View style={tw`mt-5 flex items-end`}>
-              <TouchableOpacity
-                style={tw`px-3 py-3 bg-gray-800 rounded flex justify-center items-center w-4/12`}
-              >
-                <Text style={tw`text-white font-semibold`}>CREATE</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
+        <TaskInput refetch={refetch} user={user}/>
 
         <View style={tw`flex-1`}>
           <ScrollView style={tw`p-4`}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {todos.map((todo) => {
+              return (
+                <View key={todo._id}>
+                  <Card data={todo} refetch={refetch} />
+                </View>
+              );
+            })}
           </ScrollView>
         </View>
       </View>
