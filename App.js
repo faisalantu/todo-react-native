0fbba228a5d292d222b680b756617d4a1d57bd6a
faisalantu <faisalantu@gmail.com>
2022-06-21 15:38:28 +0600

    🍈 React query to app

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './Screens/HomeScreen';
-import LoginScreen from './Screens/LoginScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./Screens/HomeScreen";
+import LoginScreen from "./Screens/LoginScreen";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+import SignupScreen from "./Screens/SignupSceeen";
 
 const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{headerShown:false}} name="Home" component={HomeScreen} />
-        <Stack.Screen options={{headerShown:false}} name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name='Login'
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name='Home'
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name='Signup'
+            component={SignupScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
